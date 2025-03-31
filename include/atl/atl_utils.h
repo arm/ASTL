5f97a7b0098e9e5329705af72282239369b53dcb
Brendan Long <bccbrendan@gmail.com>
2025-03-31 12:18:08 -0500

    [NO-JIRA] Create multi-OS build action (#4)
    ## Description
    Use a matrix of ubuntu and windows OSs, and GCC, cl, and clang compilers
    to build the project and run ctest.
    
    This is mostly based on a GitHub C++ template workflow. I figure it's a
    good starting point to have the build success/failure and ctest test
    entry point visible on PRs early in this project.
    
    ## Customer Impact
    Closer to an automated CI/CD pipeline. This infrastructure should
    promote a faster path from code commits to updated library binaries.
    
    ## Performance Impact
    N/A
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [X] New feature (non-breaking change which adds functionality)
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    
    - [x] View workflow results in PR - ensure builds are successful for
    both OSs, and all 3 compilers.
    - [x] TBD: view ctest output for a basic test case.
    
    
    ## Checklist:
    
    - [X] My code follows the style guidelines of this project
    - [X] I have performed a self-review of my own code
    - [X] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [X] Any dependent changes have been merged and published in downstream
    modules
    - [X] I have checked my code and corrected any misspellings

--- include/atl/atl_utils.h
+++ include/atl/atl_utils.h
@@ -1,7 +1,23 @@
 #ifndef ATL_UTILS_H_
 #define ATL_UTILS_H_
 
-#define ATL_EXPORT __attribute__ ((visibility ("default")))
+// define a `ATL_API` macro to define visibility for API functions
+#ifdef _WIN32
+  #ifdef ATL_BUILD
+    // building the ATL library, as opposed to including it
+    #define ATL_API __declspec(dllexport)
+  #else
+    // using the ATL API
+    #define ATL_API __declspec(dllimport)
+  #endif
+#else
+  #if __GNUC__ >= 4
+    #define ATL_API __attribute__ ((visibility ("default")))
+  #else
+    // older compilers - fall back to empty macro def
+    #define ATL_API
+  #endif
+#endif
 
 #endif // ATL_UTILS_H_
 
