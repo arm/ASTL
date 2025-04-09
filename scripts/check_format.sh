48548b6022a584b8a9609360f80d195e882dcffb
Brendan Long <bccbrendan@gmail.com>
2025-04-09 16:46:32 -0500

    Detect and prompt developer if clang-format is missing (#19)
    # Prompt developer to install clang-format if missing
    
    ## Description
    
    the scripts/format.sh and scripts/check_format.sh rely on clang-format.
    This was silently failing on machines where it wasn't installed
    
    ## Customer Impact
    None
    ## Performance Impact
    
    None
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [X] Bug fix (non-breaking change which fixes an issue)
    
    ## How Has This Been Tested?
    
    Please describe the tests that you ran to verify your changes. Provide
    instructions so we can reproduce. Please also list any relevant details
    for your test configuration
    
    - [X] On a macbook before clang-format was installed (prompt gives good
    installation instructions)
    - [X] On a macbook with clang-format installed via brew (script works as
    before)
    - [X] On astra1 - no change (which is expected, since clang-format was
    installed there already)
    
    **Test Configuration**:
    * OS name:
    * OS version:
    * Kernel version:
    * Hardware name:
    * Hardware stepping:
    * Firmware version:
    * Machine name:
    
    ## Checklist:
    
    - [X] My code follows the style guidelines of this project
    - [X] I have performed a self-review of my own code
    - [X] I have commented my code, particularly in hard-to-understand areas
    - [X] I have made corresponding changes to the documentation
    - [X] My changes generate no new warnings
    - [X] I have added tests that prove my fix is effective or that my
    feature works
    - [X] New and existing unit tests pass locally with my changes
    - [X] Any dependent changes have been merged and published in downstream
    modules
    - [X] I have checked my code and corrected any misspellings
    
    Ossmosis-Source-Commit: b2c845beaba7bf1629425b2f3a3da7ac95dc3f0c

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -4,10 +4,21 @@
 # run this from the repo root
 set -e
 
+# Check for clang-format
+if ! command -v clang-format >/dev/null 2>&1; then
+    echo "❌ clang-format is not installed."
+    echo "👉 Please install it with:"
+    echo "   sudo apt install clang-format        # Debian/Ubuntu"
+    echo "   brew install clang-format            # macOS (Homebrew)"
+    echo "   pacman -S clang                      # Arch"
+    exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPT_DIR/get_find_file_expressions.sh  # define PRUNE_EXPR
 
 FILES=$(find $(realpath .) \( $PRUNE_EXPR \) -prune -o \( -type f \( $NAME_ALL_SOURCES_AND_HEADERS  \) \) -print)
+
 
 if ! clang-format --dry-run --Werror $FILES; then
   echo "💥 Code is not properly formatted! Run 'cmake --build . --target format'"
