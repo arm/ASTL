81a204a33c5a892cdcbdbb5de0b543814a46e22e
Faycal Benmlih <fayssal.benmlih@arm.com>
2025-05-05 14:09:12 -0500

    [ASTL-50] Header utility for logging and writing output files (#41)
    ## Description
    
    Logging utility for writing formatted log messages with severity level
    and source location to the console or to a file. The utility can also be
    used to log preformated text strings for output generation
    
    Fixes # (ASTL-50)
    
    ## Customer Impact
    
    Utility would be used as part of the development process and will also
    be used to log errors and critical failures to the user
    
    ## Performance Impact
    
    None known
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Interface change (fix or feature that would cause the user facing
    interface to change)
    - [ ] This change requires a documentation update
    - [ ] This changes requires a version update
    
    ## How Has This Been Tested?
    
    Manual tests with combination of console and file logging as well as
    environement variables
    TODO: unit testing in separate patch.
    
    **Test Configuration**:
    * OS name: Ubuntu
    * OS version: 24.04
    * Kernel version: 6.12.10
    * Machine name: asct-astra2
    
    ## Checklist:
    
    - [X] My code follows the style guidelines of this project
    - [X] I have performed a self-review of my own code
    - [X] I have commented my code, particularly in hard-to-understand areas
    - [X] I have made corresponding changes to the documentation
    - [X] My changes generate no new warnings
    - [X] I have added tests that prove my fix is effective or that my
    feature works
    - [X] New and existing unit tests pass locally with my changes
    - [X] Any dependent changes have been merged and published in downstream
    modules
    - [X] I have checked my code and corrected any misspellings
    
    ---------
    
    Co-authored-by: Faycal Benmlih <fayssal.benmlih@arm.com>
    
    Ossmosis-Source-Commit: 8c219a297b57605ab32bee3da4b69f5d8e12e495

---
+++ utils/astl_utils.hpp
@@ -0,0 +1,88 @@
+#ifndef INCLUDE_ASTL_UTILS_HPP_
+#define INCLUDE_ASTL_UTILS_HPP_
+
+#include <algorithm>
+#include <random>
+
+#include "astl/astl_errors.h"
+
+namespace astl {
+
+/* @brief Set specified environment variable to the specified value
+ *
+ * @param var_name  The name of the environment variable to set
+ * @param var_value The value to set the environment variable to
+ *
+ * @return astl_status_code
+ */
+inline astl_status_code SetEnvVar(const std::string& var_name, const std::string& var_value) {
+  astl_status_code status = ASTL_STATUS_INTERNAL_ERROR;
+  if (var_name.empty()) {
+    return status;
+  }
+#ifndef _WIN32  // Linux
+  status = (setenv(var_name.c_str(), var_value.c_str(), 1) != 0) ? status : ASTL_STATUS_SUCCESS;
+#else  // Windows
+  std::string env_value = var_name + "=" + var_value;
+  status                = (_putenv(env_value.c_str()) != 0) ? status : ASTL_STATUS_SUCCESS;
+#endif
+
+  return status;
+}
+
+/* @brief Get environment variable value
+ * @param var_name  The environment variable name
+ *
+ * @return the value of the environment variable if found or empty std::string()
+ */
+inline std::string GetEnvVar(const std::string& var_name) {
+  if (var_name.empty()) {
+    return std::string();
+  }
+#ifndef _WIN32  // Linux
+  const char* var = getenv(var_name.c_str());
+  return (var == nullptr) ? std::string() : std::string(var);
+#else  // Windows
+  char* var_value = nullptr;
+  if (_dupenv_s(&var_value, nullptr, var_name.c_str()) != 0 || var_value == nullptr) {
+    return std::string();
+  }
+  std::string var(var_value);
+  free(var_value);
+  return var;
+#endif
+}
+
+/* @brief Check if an environment variable is set
+ *
+ * @prarm var_name  The environment variable name
+ *
+ * @return true if environment variable is set, false otherwise
+ * Note: An environment variable is considered set if it is present and it is set to
+ * any value other than empty string, 0, off, no, false or their uppercase version
+ */
+inline bool IsEnvVarSet(const std::string& var_name) {
+  std::string var = GetEnvVar(var_name);
+  std::transform(var.begin(), var.end(), var.begin(), [](unsigned char character) { return std::toupper(character); });
+  bool var_set = true;
+  if (var.empty() || var == "0" || var == "OFF" || var == "NO" || var == "FALSE") {
+    var_set = false;
+  }
+
+  // Environment variable is set but not set to any of the expected off values
+  return var_set;
+}
+
+/* @brief Generate a random number
+ *
+ * @return uint64_t random value
+ */
+inline uint64_t GetRandomNumber() {
+  static std::mt19937                            prng(std::random_device{}());  // pseudorandom number generator
+  static std::uniform_int_distribution<uint64_t> rand_num(0);                   // random number
+  return rand_num(prng);
+}
+
+}  // namespace astl
+
+#endif /* INCLUDE_ASTL_UTILS_HPP_ */
