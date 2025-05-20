187c42f6e2053f52a75700c5ee1adc9f2c13b18e
Brendan Long <bccbrendan@gmail.com>
2025-05-20 16:21:39 -0500

    [NO-JIRA] Add project target dependency graph (#67)
    ## Description
    
    Add 'dependency_graph' .dot and .svg files to doc/ folder, along with a
    scripts/update_diagrams.sh script to update them.
    This is output from CMake, documenting our project's targets and the
    libraries (including 3rd party) they depend on.
    I found it useful when debugging linker errors with fmt and spdlog
    
    Cmake outputs the .dot file, which is a text-based description of the
    dependency graph. `dot` can be used to convert this to an image (svg)
    for easy preview in Github. We could consider embedding this in the
    README.md, but i think it's more detailed that most readers would need.
    
    Potential future steps:
    - extend with header-only dependencies (for instance, `trompeloeil` is a
    header dependency of wrapper_test, but it's not noted here. This could
    help us more easily track license issues.
    - add pre-commit or CI step to ensure graphs are updated (it's only
    manual for now)
    - look into converting output to mermaid format, just for consistency. I
    tried 2 tools (https://pypi.org/project/dot2mermaid/ and
    https://github.com/monitorian/dot2mermaid) without luck.
    
    ## Customer Impact
    
    None
    
    ## Performance Impact
    
    None
    
    ## Type of change
    
    - [x] This change requires a documentation update
    
    ## How Has This Been Tested?
    
    Local testing on Macbook. svg viewd in github. dot file previewed in
    VScode.
    
    ## Checklist:
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] Any dependent changes have been merged and published in downstream
    modules
    - [x] I have checked my code and corrected any misspellings
    
    Ossmosis-Source-Commit: 55b5cf28201889d494907f579a4c8a6ae2efdee4

---
+++ scripts/update_diagrams.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This script checks if the code is formatted according to clang-format rules.
+# run this from the repo root
+set -eu -o pipefail
+
+# Check for dot (from the graphviz package)
+if ! command -v dot >/dev/null 2>&1; then
+    echo "❌ dot is not installed."
+    echo "👉 Please install the graphviz package with:"
+    echo "   sudo apt install graphviz   # Debian/Ubuntu"
+    echo "   brew install graphviz       # macOS (Homebrew)"
+    exit 1
+fi
+
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# set cwd to repo root
+cd ${SCRIPT_DIR}/..
+
+cmake -S . -B build/graphviz --graphviz=build/graphviz/graph.dot > /dev/null
+if diff build/graphviz/graph.dot doc/dependency_graph.dot > /dev/null; then
+    echo "✅ No changes to the dependency graph!"
+    exit 0
+fi
+
+cp build/graphviz/graph.dot doc/dependency_graph.dot
+dot -Tsvg -o doc/dependency_graph.svg doc/dependency_graph.dot
+echo "✨ docs/dependency_graph.svg ✨ has been updated!"
