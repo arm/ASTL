1928a32ef2993c3ce8ac9710e61aee3c40b96015
Brendan Long <bccbrendan@gmail.com>
2025-08-21 08:18:22 -0500

    [ASTL-155] libsensors experiments (#188)
    ## Description
    
    Adds an executable under 'tools' that uses the libsensors library to
    display power and temperature info on the platform.
    This is preliminary pathfinding work to support a HWMON / libsensors
    collector type. It's meant to simply provide some starter code for
    future work.
    
    Fixes # [ASTL-155]
    
    ## Customer Impact
    
    None
    
    ## Performance Impact
    
    None
    
    ## Type of change
    
    - [X] New feature (non-breaking change which adds functionality)
    
    ## How Has This Been Tested?
    
    Confirm through CI that this doesn't prevent compilation on platforms
    without libsensors.
    run example and confirm that output:
    ```
    brelon01@asct-astra2:~/code/ASTL$ ./build/debug/bin/libsensors_example 
    
    Scanning apm_xgene-isa-0000 for features
    Sensor: SoC Temperature = 38°C
    Sensor: CPU power = 19.76 W
    Sensor: IO power = 14.073 W
    
    Scanning bnxt_en-pci-20300 for features
    Sensor: temp1 = 56°C
    
    Scanning nvme-pci-20100 for features
    Sensor: Composite = 51.85°C
    Sensor: Sensor 2 = 54.85°C
    
    Scanning iwlwifi_1-virtual-0 for features
    
    Scanning bnxt_en-pci-20301 for features
    Sensor: temp1 = 56°C
    
    Scanning nvme-pci-40100 for features
    Sensor: Composite = 34.85°C
    Sensor: Sensor 2 = 53.85°C
    ```
    
    Looks reasonably similar to output from builtin `sensors` command on
    astra2.
    ```
    brelon01@asct-astra2:~/code/ASTL$ sensors
    apm_xgene-isa-0000
    Adapter: ISA adapter
    SoC Temperature:  +38.0°C  
    CPU power:        19.40 W  
    IO power:         14.07 W  
    
    bnxt_en-pci-20300
    Adapter: PCI adapter
    temp1:        +56.0°C  
    
    nvme-pci-20100
    Adapter: PCI adapter
    Composite:    +51.9°C  (low  = -20.1°C, high = +83.8°C)
                           (crit = +88.8°C)
    Sensor 2:     +54.9°C  
    
    iwlwifi_1-virtual-0
    Adapter: Virtual device
    temp1:            N/A  
    
    bnxt_en-pci-20301
    Adapter: PCI adapter
    temp1:        +56.0°C  
    
    nvme-pci-40100
    Adapter: PCI adapter
    Composite:    +34.9°C  (low  = -20.1°C, high = +83.8°C)
                           (crit = +88.8°C)
    Sensor 2:     +53.9°C  
    ```
    
    
    ## Test Configuration
    
    ASTRA2 platform
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] Any dependent changes have been merged and published in downstream
    modules
    - [x] I have checked my code and corrected any misspellings

---
+++ tools/libsensors_example/src/main.cpp
@@ -0,0 +1,61 @@
+
+// based on https://linux.die.net/man/3/libsensors
+// requires package 'libsensors-dev'
+
+#include <sensors/sensors.h>
+
+#include <array>
+#include <iostream>
+
+void HandleTempSensor(const sensors_chip_name* chip, const sensors_feature* feature) {
+  const sensors_subfeature* sub = sensors_get_subfeature(chip, feature, SENSORS_SUBFEATURE_TEMP_INPUT);
+  if (sub && sub->flags & SENSORS_MODE_R) {
+    double temp = 0.0;
+    if (sensors_get_value(chip, sub->number, &temp) == 0) {
+      std::cout << "Sensor: " << sensors_get_label(chip, feature) << " = " << temp << "°C\n";
+    }
+  }
+}
+
+void HandlePowerSensor(const sensors_chip_name* chip, const sensors_feature* feature) {
+  const sensors_subfeature* sub = sensors_get_subfeature(chip, feature, SENSORS_SUBFEATURE_POWER_INPUT);
+  if (sub && sub->flags & SENSORS_MODE_R) {
+    double power = 0.0;
+    if (sensors_get_value(chip, sub->number, &power) == 0) {
+      std::cout << "Sensor: " << sensors_get_label(chip, feature) << " = " << power << " W\n";
+    }
+  }
+}
+
+int main() {
+  // Initialize sensors
+  if (sensors_init(nullptr) != 0) {
+    std::cerr << "Failed to initialize libsensors.\n";
+    return 1;
+  }
+
+  const sensors_chip_name* chip       = nullptr;
+  int                      chip_index = 0;
+  while ((chip = sensors_get_detected_chips(nullptr, &chip_index))) {
+    const sensors_feature*            feature              = nullptr;
+    int                               sensor_feature_count = 0;
+    constexpr size_t                  max_name_length      = 200;
+    std::array<char, max_name_length> chip_name{'\0'};
+    sensors_snprintf_chip_name(chip_name.data(), max_name_length, chip);
+    std::cout << "\nScanning " << chip_name.data() << " for features\n";
+    while ((feature = sensors_get_features(chip, &sensor_feature_count))) {
+      switch (feature->type) {
+        case SENSORS_FEATURE_TEMP:
+          HandleTempSensor(chip, feature);
+          break;
+        case SENSORS_FEATURE_POWER:
+          HandlePowerSensor(chip, feature);
+          break;
+        default:
+          std::cout << "Skipping unrecognized feature: " << sensors_get_label(chip, feature) << "\n";
+      }
+    }
+  }
+  sensors_cleanup();
+  return 0;
+}
