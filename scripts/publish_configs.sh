9624edc088672b11049317a51a53ddf750730440
cgardner65536 <conor.gardner@arm.com>
2026-03-16 14:38:44 -0500

    [ASTL-309] Implement Alias Table for ATX (#377)
    Create an alias table which allows the CLI app to use different names
    for metrics than what we define in our metric configs.
    
    - The alias table is completely separate from the ASTL library, and is
    associated with the ATX CLI app instead. Therefore, I have put the
    pre-publish version of the table at tools/atx/config/alias_table.json
    - For testing purposes, the easiest way to point to this file is by
    setting the environment variable `export ALIAS_TABLE_PATH=$(realpath
    tools/atx/config/alias_table.json)`. This path has a corresponding
    reasonable default value, cli arg, and config toml entry as well. See
    c19e17059a9ce9b0751524754920c30beffdb8db
    - Aliases are now displayed when you call `build/debug/bin/atx --help`
    - By default, if an alias isn't found for a metric, the metric is
    hidden, as discussed on Thurs Mar 12 2026.
    - If the user also supplies the --expert-mode cli arg, then alias names
    will be displayed preferentially, but the true metric name will be
    displayed as a faillback. To make this distinction clear, when a true
    metric name is displayed, it is printed with [brackets] around the name.
    - When running a collection, you can pass either an alias string or the
    true metric name via the `-m/--metric` cli argument. I figured there's
    no reason to artificially fail when the true metric name is passed,
    since this doesn't overload the user with any extra info, but does make
    the cli app more robust for expert users
    - When printing a report after a collection, the aliased name is used
    preferentially. If a metric doesn't have an alias, the true name is used
    as a fallback.
    
    - The current alias table only has entries for MockSysfs. I didn't
    pre-fill data yet since I don't have a good way to test it.
    - The keys in alias_table.json are not final. I just converted the
    metric name to lowercase and prepended "alias." to make it clear that
    the aliasing feature was working during manual runs. Created ASTL-347 to
    think of some actual good names for our aliases.
    - scripts/publish_configs.sh does not handle the new
    tools/atx/config/alias_table.json at all ASTL-348
    - Our release flow / OOBT does not package the new
    tools/atx/config/alias_table.json
    - There is no automated testing (integration / unit) for the ATX CLI App
    ASTL-335
    
    Customer can see a potentially more uniform set of metrics across
    different platforms and with different data sources
    (scmi, lm-sensors, etc)
    
    Untested, but there is now a new json deserilization step (done once per
    execution) and a hash lookup each time a metric name if referenced from
    the CLI app.
    
    The STL library is not affected.
    
    - [X] New feature (non-breaking change which adds functionality)
    - [X] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    TODO - Will need manual tests
    
    - OS name: Ubuntu
    - OS version: 24.04, 22.04
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [x] Any dependent changes have been merged and published in downstream
    modules
    - [x] I have checked my code and corrected any misspellings
    
    Ossmosis-Source-Commit: e72a8718d9d0887d641279874215b5762bacdcdb

--- scripts/publish_configs.sh
+++ scripts/publish_configs.sh
@@ -11,6 +11,9 @@ set -eu -o pipefail
 # 1. Filters out confidential content based on --confidential flag
 # 2. Excludes JSON files with document.confidential == true
 # 3. Removes JSON elements within files that have "confidential": true
+#
+# @TODO(ASTL-348) Properly render alias_table.json to filter out irrelevent/confidential IPs
+# @TODO(ASTL-351) Consider tradeoffs of .toml vs .json for the alias table
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
